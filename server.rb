f0e46535a70c13c69dd76f217af2c11f8d4296ae
gorums <acksecurity@hotmail.com>
2017-01-18 02:13:02 -0500

    Fix primjs

--- server.rb
+++ server.rb
@@ -33,14 +33,11 @@ __END__
 
 @@ index
 %script{ :src => '//ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js', :type => 'text/javascript', :charset => 'utf-8' }
-%script{ :src => 'prism.js', :type => 'text/javascript', :charset => 'utf-8' }
 %link{:rel => :stylesheet, :type => :"text/css", :href => "prism.css"}
 %h1 Introduction
 %p A quine is a non-empty computer program which takes no input and produces a copy of its own source code as its only output. The standard terms for these programs in the computability theory and computer science literature are "self-replicating programs", "self-reproducing programs", and "self-copying programs".
 
-%pre.line-numbers
-  %code.language-ruby
-    %div.main-section
+%div.main-section
 
 :javascript
     $(document).ready( function() {
@@ -55,7 +52,9 @@ __END__
               format: 'json'
             },
             success: function(data) {
-                 $('.main-section').replaceWith(JSON.parse(data).code);
+                 prettyCode = '<pre class="line-number"><code class="language-ruby">' + JSON.parse(data).code + '</code></pre>';
+                 $('.main-section').replaceWith(prettyCode);
+                 $.getScript("prism.js");
             }
           })
         }
